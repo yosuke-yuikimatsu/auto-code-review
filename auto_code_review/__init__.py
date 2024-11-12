6ceb5245b5d1ff92f4370a6f228cc1ba26aa415a
Dmitry-Geyvandov <geyvandovdd@gmail.com>
2024-11-12 00:19:01 +0000

    Ai Analyzer tests and tests runner added

--- auto_code_review/__init__.py
+++ auto_code_review/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
 
-##from .core import Reviewer
+from .core import Reviewer
 from .github_client import GitHubClient
-##from .ai_analyzer import AIAnalyzer
+from .ai_analyzer import AIAnalyzer
