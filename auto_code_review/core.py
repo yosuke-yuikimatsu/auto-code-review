16233402dbffc844437fbf790adbcec561093298
Dmitry-Geyvandov <geyvandovdd@gmail.com>
2024-11-13 12:00:04 +0000

    Posting inline comments fixed

--- auto_code_review/core.py
+++ auto_code_review/core.py
@@ -60,7 +60,7 @@ class Reviewer:
                     extension = ext
                     break
             else:
-                extension = None
+                continue
 
             comments = self.analyzer.analyze_diff(patch,extension) # Generate code-review for a changed file via ChatGPT prompt
             for position, comment in comments:
