d0ee0b8416cea8ec6930903085dac5920fbd1ad1
Dmitry-Geyvandov <geyvandovdd@gmail.com>
2024-12-17 15:29:39 +0000

    hehehehe

--- auto_code_review/ai_analyzer.py
+++ auto_code_review/ai_analyzer.py
@@ -6,12 +6,9 @@ from jinja2 import Environment,FileSystemLoader
 import os
 from pydantic import BaseModel
 
-class InlineComment(BaseModel) :
-    line : int
-    comment : str
 
 class Response(BaseModel) :
-    inline_comments : tp.List[InlineComment]
+    inline_comments : tp.List[tp.Tuple[int,str]]
 
 class AIAnalyzer:
     def __init__(self, api_key : str, settings : tp.Dict):
